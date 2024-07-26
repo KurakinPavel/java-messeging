69140350c3fad70a398dd0b343313b2bdb2494c9
KurakinPavel <pu.kurakin@yandex.ru>
2024-07-26 19:56:58 +0300

    Kafka: KafkaTemplate ver 0

---
+++ src/main/java/ru/kurakin/kafkatemplate/KafkatemplateApplication.java
@@ -0,0 +1,13 @@
+package ru.kurakin.kafkatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkatemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkatemplateApplication.class, args);
+	}
+
+}
