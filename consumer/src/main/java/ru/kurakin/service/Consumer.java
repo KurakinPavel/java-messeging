eba3dd91d63013df15d07d58f695c09f0b271fa0
KurakinPavel <pu.kurakin@yandex.ru>
2024-07-27 18:05:35 +0300

    Kafka: replying template in docker ver. 1.1

--- consumer/src/main/java/ru/kurakin/service/Consumer.java
+++ consumer/src/main/java/ru/kurakin/service/Consumer.java
@@ -1,7 +1,6 @@
 package ru.kurakin.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
